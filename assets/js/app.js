ecc7b8a7fa77c13076112efa6fb2bf52f8c715e0
Jaime M <jaimemarsh19@gmail.com>
2026-01-27 19:44:57 -0500

    added photos, new layout, new colors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
 //annimation transition for all objects 'hide'
-const observer = new IntersectionObserver((enteries) => {
-    enteries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
-const hiddenElements = document.querySelectorAll('.hide');
-hiddenElements.forEach((el) => observer.observe(el));
+// const observer = new IntersectionObserver((enteries) => {
+//     enteries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+// const hiddenElements = document.querySelectorAll('.hide');
+// hiddenElements.forEach((el) => observer.observe(el));
 
